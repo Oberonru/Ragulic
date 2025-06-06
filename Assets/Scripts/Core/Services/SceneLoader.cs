a7d3ac8c48c848517102858d0e324260737c1f9a
Alexey <Oberonru@mail.ru>
2025-06-06 15:25:38 +0600

    Create SceneLoader Service

--- Assets/Scripts/Core/Services/SceneLoader.cs
+++ Assets/Scripts/Core/Services/SceneLoader.cs
@@ -1,11 +1,34 @@
 using System.Providers.Services;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Core.Services
 {
+    [CreateAssetMenu(menuName = "System/Service/SceneLoader", fileName = "Scene Loader")]
     public class SceneLoader : ScriptableService
     {
-        public void LoadingScene(string sceneName) {}
+        public void LoadingScene(string sceneName)
+        {
+            UniTask.Void(async () => await LoadSceneAsync("Boot"));
+            UniTask.Void(async () => await LoadSceneAsync(sceneName));
+        }
 
+        private async UniTask LoadSceneAsync(string sceneName)
+        {
+            if (SceneManager.GetActiveScene().name == sceneName) return;
+
+            AsyncOperation delay = SceneManager.LoadSceneAsync(sceneName);
+
+            while (!delay.isDone)
+            {
+                await UniTask.Yield();
+
+                if (delay.progress >= 90)
+                {
+                    delay.allowSceneActivation = true;
+                }
+            }
+        }
     }
 }
