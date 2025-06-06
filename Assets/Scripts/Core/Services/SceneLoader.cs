67e8507eee8e98df4768efbc79d29985d128d079
Alexey <Oberonru@mail.ru>
2025-06-06 15:06:54 +0600

    Add NuGetForUnity, UniRx, UniTask

---
+++ Assets/Scripts/Core/Services/SceneLoader.cs
@@ -0,0 +1,11 @@
+using System.Providers.Services;
+using Cysharp.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class SceneLoader : ScriptableService
+    {
+        public void LoadingScene(string sceneName) {}
+
+    }
+}
