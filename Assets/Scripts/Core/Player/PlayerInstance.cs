8a96317c7f8706399a0f4e8ca6033c11dcb86c4f
Alexey <30863409+Oberonru@users.noreply.github.com>
2025-06-08 15:13:38 +0600

    Edit injection PlayerConfig in to Player Instance

--- Assets/Scripts/Core/Player/PlayerInstance.cs
+++ Assets/Scripts/Core/Player/PlayerInstance.cs
@@ -2,13 +2,14 @@ using System;
 using Core.Configs.Player;
 using Core.Player.Components;
 using UnityEngine;
+using Zenject;
 
 namespace Core.Player
 {
     [RequireComponent(typeof(PlayerMovement))]
     public class PlayerInstance : MonoBehaviour
     {
-        [SerializeField] private PlayerConfig _playerStats;
+        [Inject] private PlayerConfig _playerStats;
         [SerializeField] private PlayerMovement _movement;
 
         public PlayerConfig Stats => _playerStats;
