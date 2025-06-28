546772a2e27d395acbfbcae0d66ff4e3cd7d2920
Aleksey <Oberonru@mail.ru>
2025-06-28 21:45:09 +0600

    Bindig PoolMono width EffectFactory

--- Assets/Scripts/Core/Factories/EffectFactory.cs
+++ Assets/Scripts/Core/Factories/EffectFactory.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Pool;
 using Core.Effects;
 using UnityEngine;
 using Zenject;
@@ -7,10 +9,28 @@ namespace Core.Factories
     public class EffectFactory : IEffectFactory
     {
         [Inject] private DiContainer _container;
-        
-        public IEffectInstance Create(EffectInstance prefab, Vector3 position, Quaternion rotation, Transform parent = null)
+        private Dictionary<EffectInstance, PoolMono<EffectInstance>> _pool = new();
+
+        private const int Count = 30;
+
+        public IEffectInstance Create(EffectInstance prefab, Vector3 position, Quaternion rotation,
+            Transform parent = null)
         {
-           return _container.InstantiatePrefabForComponent<EffectInstance>(prefab, position, rotation, parent);
+            if (!_pool.TryGetValue(prefab, out PoolMono<EffectInstance> pool))
+            {
+                PoolMono<EffectInstance> newPool =
+                    new PoolMono<EffectInstance>(prefab, prefab.transform, _container, Count, true, false);
+                _pool.Add(prefab, newPool);
+
+                //Перед возвратом установить позицию и ротацию элемента, сейчас она в нулевых
+                var freeElement = newPool.GetFreeElement();
+                
+                return freeElement;
+            }
+            else
+            {
+                return pool.GetFreeElement();
+            }
         }
     }
 }
