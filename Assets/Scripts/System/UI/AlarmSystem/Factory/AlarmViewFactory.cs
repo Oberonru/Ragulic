be381a2ccaa1d73abf0750378917a22ffa2508bf
Yaroslav <30863409+Oberonru@users.noreply.github.com>
2025-07-24 17:46:24 +0600

    Clear pool if load new scene

--- Assets/Scripts/System/UI/AlarmSystem/Factory/AlarmViewFactory.cs
+++ Assets/Scripts/System/UI/AlarmSystem/Factory/AlarmViewFactory.cs
@@ -2,6 +2,7 @@ using System.Pool;
 using System.UI.AlarmSystem.Views;
 using UniRx;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace System.UI.AlertSystem.Factory
@@ -12,25 +13,32 @@ namespace System.UI.AlertSystem.Factory
         private PoolMono<AlarmView> _pool;
 
         public IObservable<IAlarmView> OnSpawn => _onSpawn;
-        private Subject<AlarmView> _onSpawn = new ();
+        private Subject<AlarmView> _onSpawn = new();
 
         public IAlarmView Create(AlarmView prefab, Vector3 position, Quaternion rotation, Transform parent = null)
         {
             if (_pool == null)
             {
+                SceneManager.sceneUnloaded += OnSceneUnloaded;
                 _pool = new PoolMono<AlarmView>(prefab, null, _di, 10, true);
             }
 
             var view = _pool.GetFreeElement();
-            
+
             if (parent != null)
             {
                 view.transform.SetParent(parent, false);
             }
-            
+
             _onSpawn.OnNext(view);
-            
+
             return view;
+        }
+
+        private void OnSceneUnloaded(Scene scene)
+        {
+            _pool = null;
+            SceneManager.sceneUnloaded -= OnSceneUnloaded;
         }
     }
 }
