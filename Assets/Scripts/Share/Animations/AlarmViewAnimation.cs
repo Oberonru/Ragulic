b8e490baa8a7f4dedf58ed9aa33d41378e5846f4
Yaroslav <30863409+Oberonru@users.noreply.github.com>
2025-07-25 14:35:31 +0600

    Create fade and deactivate alarm view

--- Assets/Scripts/Share/Animations/AlarmViewAnimation.cs
+++ Assets/Scripts/Share/Animations/AlarmViewAnimation.cs
@@ -1,16 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using Share.Configs;
 using UnityEngine;
 using Zenject;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
 
 namespace Share.Animations
 {
     public class AlarmViewAnimation : MonoBehaviour
     {
         [Inject] private AlarmViewAnimationConfig _config;
+        [SerializeField] private Image _icon;
+        [SerializeField] private TextMeshProUGUI _alarmInfo;
 
-        private void OnEnable()
+        private Sequence _startSequence;
+        private Sequence _endSequence;
+
+        private async void OnEnable()
         {
-            
+            _endSequence?.Kill();
+            _startSequence?.Kill();
+            _icon.DOFade(0, 0);
+            _alarmInfo.DOFade(0, 0);
+
+            _startSequence = DOTween.Sequence();
+            _startSequence.Append(_icon.DOFade(1, _config.Speed / 2));
+            _startSequence.Join(_alarmInfo.DOFade(1, _config.Speed / 2));
+
+            _startSequence.OnComplete(() => PlayFade());
+        }
+
+        private async void PlayFade()
+        {
+            var token = this.GetCancellationTokenOnDestroy();
+            TimeSpan span = TimeSpan.FromSeconds(_config.Duration);
+
+            await UniTask.Delay(span, cancellationToken: token);
+
+            _endSequence = DOTween.Sequence();
+            _endSequence.Append(_icon.DOFade(0, _config.Speed));
+            _endSequence.Join(_alarmInfo.DOFade(0, _config.Speed));
+
+            _endSequence.SetEase(_config.Ease);
+
+            _endSequence.Play();
+            _endSequence.OnComplete(() => Deactivate());
+        }
+
+        private async void Deactivate()
+        {
+            var token = this.GetCancellationTokenOnDestroy();
+            TimeSpan span = TimeSpan.FromSeconds(_config.DelayDeactivate);
+
+            await UniTask.Delay(span, cancellationToken: token);
+            gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Share/Configs/AlarmViewAnimationConfig.cs
+++ Assets/Scripts/Share/Configs/AlarmViewAnimationConfig.cs
@@ -1,15 +1,20 @@
 using System.Providers.Configs;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Share.Configs
 {
     [CreateAssetMenu(menuName = "Configs/Share/AlarmViewAnimationConfig", fileName = "AlarmViewAnimationConfig")]
     public class AlarmViewAnimationConfig : ScriptableConfig
     {
-        [SerializeField] private float _duration;
+        [SerializeField] private float _duration = 3f;
         [SerializeField] private float _speed;
-        
+        [SerializeField] private float _delayDeactivate = 0.08f;
+        [SerializeField] private Ease _ease = Ease.Linear;
+
         public float Duration => _duration;
         public float Speed => _speed;
+        public float DelayDeactivate => _delayDeactivate;
+        public Ease Ease => _ease;
     }
 }
